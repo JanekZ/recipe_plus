e132a3e6f119d7492de694b6335897e64cf59a2d
Filip Cielibała <252788@edu.p.lodz.pl>
2026-06-16 17:22:53 +0200

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import MainPage from './components/MainPage/MainPage.tsx';
-import MyRecipePage from './components/MyRecipePage/MyRecipePage.tsx';
-import MyProductsPage from './components/MyProductsPage/MyProductsPage.tsx';
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './components/MainPage/MainPage.tsx'
+import MyRecipePage from './components/MyRecipePage/MyRecipePage.tsx'
+import MyProductsPage from './components/MyProductsPage/MyProductsPage.tsx'
+import LoginPage from './components/Login/LoginPage.tsx'
+import AccountPage from './components/Account/AccountPage.tsx'
+import RecipeEditor from './components/RecipeEditor/RecipeEditor.tsx'
+import RecipeShowcase from './components/RecipeShowcase/RecipeShowcase.tsx'
 import './App.css'
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/moje-przepisy" element={<MyRecipePage />} />
-        <Route path="/moje-produkty" element={<MyProductsPage />} />
-      </Routes>
-  );
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/konto" element={<AccountPage />} />
+      <Route path="/moje-przepisy" element={<MyRecipePage />} />
+      <Route path="/moje-produkty" element={<MyProductsPage />} />
+      <Route path="/nowy-przepis" element={<RecipeEditor />} />
+      <Route path="/edytuj-przepis/:id" element={<RecipeEditor />} />
+      <Route path="/przepis/:id" element={<RecipeShowcase />} />
+    </Routes>
+  )
 }
 
 export default App
